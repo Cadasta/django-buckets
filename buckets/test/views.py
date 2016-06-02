827b2147fd21966c2ca55d61ebc2fd1061da7a39
Oliver Roick <oliver.roick@gmail.com>
2016-06-02 12:22:20 +0200

    Fix directory

--- buckets/test/views.py
+++ buckets/test/views.py
@@ -12,10 +12,11 @@ def fake_s3_upload(request):
     ensure_dirs()
     key = request.POST.get('key')
 
-    path = os.path.join(settings.MEDIA_ROOT,
-                        's3/uploads', key[:key.rfind('/')])
-    if not os.path.exists(path):
-        os.makedirs(path)
+    if '/' in key:
+        path = os.path.join(settings.MEDIA_ROOT,
+                            's3/uploads', key[:key.rfind('/')])
+        if not os.path.exists(path):
+            os.makedirs(path)
 
     file = request.FILES.get('file')
     default_storage.save(key, file)
