53715a76105790b1e0c948f0885b5e1d4197e25e
Oliver Roick <oliver.roick@gmail.com>
2016-07-08 15:56:35 +0200

    Fix test dirs

--- buckets/test/utils.py
+++ buckets/test/utils.py
@@ -2,7 +2,7 @@ import os
 from django.conf import settings
 
 
-def ensure_dirs():
+def ensure_dirs(add=None):
     path = os.path.join(settings.MEDIA_ROOT, 's3', 'uploads')
     if not os.path.exists(path):
         os.makedirs(path)
@@ -10,3 +10,8 @@ def ensure_dirs():
     path = os.path.join(settings.MEDIA_ROOT, 's3', 'downloads')
     if not os.path.exists(path):
         os.makedirs(path)
+
+    if add:
+        path = os.path.join(settings.MEDIA_ROOT, add)
+        if not os.path.exists(path):
+            os.makedirs(path)
