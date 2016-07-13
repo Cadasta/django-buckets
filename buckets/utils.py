675364683c5415f1db7a5599d8ad97f72f69aaf0
Ian Ross <iross@cadasta.org>
2016-07-13 09:48:00 +0200

    Make random IDs start with a letter

--- buckets/utils.py
+++ buckets/utils.py
@@ -17,15 +17,24 @@ def validate_settings():
 ID_FIELD_LENGTH = 24
 
 alphabet = string.ascii_lowercase + string.digits
+alphabet0 = string.ascii_lowercase + string.ascii_lowercase
 for loser in 'l1o0':
     i = alphabet.index(loser)
     alphabet = alphabet[:i] + alphabet[i + 1:]
+for loser in 'lo':
+    i = alphabet0.index(loser)
+    alphabet0 = alphabet0[:i] + alphabet0[i + 1:]
 
 
 def byte_to_base32_chr(byte):
     return alphabet[byte & 31]
 
 
+def byte_to_letter(byte):
+    return alphabet0[byte & 31]
+
+
 def random_id():
     rand_id = [random.randint(0, 0xFF) for i in range(ID_FIELD_LENGTH)]
-    return ''.join(map(byte_to_base32_chr, rand_id))
+    return (byte_to_letter(rand_id[0]) +
+            ''.join(map(byte_to_base32_chr, rand_id[1:])))
