5c13aa1bd1db32c8372e93fe876daf7f3fc1f815
Oliver Roick <oliver.roick@gmail.com>
2017-08-04 09:21:45 +0200

    Clear errors when new file is uploaded (#34)

--- buckets/static/buckets/js/script.js
+++ buckets/static/buckets/js/script.js
@@ -38,12 +38,16 @@
         }
     }
 
+    function clearErrors(el) {
+        if (errs = el.querySelector('.errors')) { errs.remove(); }
+    }
+
     function error(el, msg) {
         message(el);
         el.querySelector('.file-url').value = '';
         el.querySelector('.file-input').value = '';
 
-        if (errs = el.querySelector('.errors')) { errs.remove(); }
+        clearErrors(el);
 
         var errorList = document.createElement('ul');
         errorList.setAttribute('class', 'errors');
@@ -58,7 +62,7 @@
         var link = el.querySelector('.file-link'),
             url = el.querySelector('.file-url');
 
-        if (errs = el.querySelector('.errors')) { errs.remove(); }
+        clearErrors(el);
 
         url.value = fileUrl;
         link.href = fileUrl;
@@ -154,6 +158,8 @@
         var el = e.target.parentElement,
             file = el.querySelector('.file-input').files[0],
             accepted = el.getAttribute('data-accepted-types');
+
+        clearErrors(el);
 
         disableSubmit(el, true);
         var ext = file.name.split('.').slice(-1)[0];
