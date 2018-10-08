488e331bf15d913f750d3801185617c7dc21591f
requires.io <support@requires.io>
2018-10-08 23:53:20 +0200

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     package_data=get_package_data(package),
     include_package_data=True,
     install_requires=[
-        'boto3==1.9.18'
+        'boto3==1.9.19'
     ],
     classifiers=[
         'Development Status :: 3 - Alpha',
